d271286ade2bf544b6c6c3e81465249777391099
Andrey Lushnikov <aslushnikov@gmail.com>
2026-03-03 12:28:03 -0700

    feat: implement basic test collection

--- src/reporter.ts
+++ src/reporter.ts
@@ -1,18 +1,21 @@
 import type { FlakinessReport as FK } from '@flakiness/flakiness-report';
 import { GitWorktree, ReportUtils, writeReport } from '@flakiness/sdk';
 import chalk from 'chalk';
+import assert from 'node:assert';
 import path from 'node:path';
-import type { TestModule, Vitest } from 'vitest/node';
+import type { TestCase, TestModule, TestSuite, Vitest } from 'vitest/node';
 import type { Reporter } from 'vitest/reporters';
 
 const warn = (txt: string) => console.warn(chalk.yellow(`[flakiness.io] ${txt}`));
 const err = (txt: string) => console.error(chalk.red(`[flakiness.io] ${txt}`));
 const log = (txt: string) => console.log(`[flakiness.io] ${txt}`);
 
-
 export default class FlakinessReporter implements Reporter {
   private _vitest!: Vitest;
   private _startTimestamp: number = Date.now();
+  private _tests = new Map<string, FK.Test>();
+  private _allSuites = new Map<string, FK.Suite>();
+  private _fileSuites = new Map<string, FK.Suite>();
 
   onInit(vitest: Vitest) {
     this._vitest = vitest;
@@ -20,6 +23,54 @@ export default class FlakinessReporter implements Reporter {
 
   onTestRunStart() {
     this._startTimestamp = Date.now();      
+  }
+
+  private _ensureFKSuite(p: TestSuite | TestModule): FK.Suite {
+    let suite = this._allSuites.get(p.id);
+    if (suite)
+      return suite;
+    if ('name' in p) {
+      const parent = this._ensureFKSuite(p.parent);
+      suite = {
+        type: 'suite',
+        title: p.name,
+      };
+      parent.suites ??= [];
+      parent.suites.push(suite);
+    } else {
+      suite = {
+        type: 'file',
+        title: p.relativeModuleId,
+      };
+      this._fileSuites.set(p.id, suite);
+    }
+    this._allSuites.set(p.id, suite);
+    return suite;
+  }
+
+  async onTestCaseResult(testCase: TestCase) {
+    let fkTest = this._tests.get(testCase.id);
+    if (!fkTest) {
+      fkTest = {
+        attempts: [],
+        title: testCase.name,
+      }
+      this._tests.set(testCase.id, fkTest);
+      const parent = this._ensureFKSuite(testCase.parent);
+      parent.tests ??= [];
+      parent.tests.push(fkTest);
+    }
+    const diag = testCase.diagnostic();
+    assert(diag, `Diagnostic must be present in finished test cases`);;
+
+    const result = testCase.result();
+    fkTest.attempts.push({
+      startTimestamp: diag.startTime as FK.UnixTimestampMS,
+      duration: diag.duration as FK.DurationMS,
+      status: result.state === 'failed' ? 'failed' : 
+        result.state === 'skipped' ? 'skipped' : 
+        'passed',
+    });
   }
 
   async onTestRunEnd(testModules: ReadonlyArray<TestModule>) {
@@ -34,7 +85,6 @@ export default class FlakinessReporter implements Reporter {
       return;
     }
 
-
     const environment = ReportUtils.createEnvironment({ name: 'default' });
     const duration = (Date.now() - this._startTimestamp) as FK.DurationMS;
     const report: FK.Report = ReportUtils.normalizeReport({
@@ -43,8 +93,7 @@ export default class FlakinessReporter implements Reporter {
       environments: [environment],
       startTimestamp: this._startTimestamp as FK.UnixTimestampMS,
       duration,
-      suites: [],
-      tests: [],
+      suites: Array.from(this._fileSuites.values()),
     });
     await ReportUtils.collectSources(worktree, report);
 
--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -1,15 +1,48 @@
+import { FlakinessReport } from '@flakiness/flakiness-report';
 import { expect, it } from 'vitest';
 import { generateFlakinessReport } from './utils';
 
+function assertSuites(suites: FlakinessReport.Suite[]|undefined, length: number): FlakinessReport.Suite[] {
+  expect(suites?.length ?? 0).toBe(length);
+  return suites!;
+}
+
+function assertTests(suite: FlakinessReport.Suite, length: number): FlakinessReport.Test[] {
+  expect(suite.tests?.length ?? 0).toBe(length);
+  return suite.tests!;
+}
+
+function assertRootSuite(report: FlakinessReport.Report, idx: number): FlakinessReport.Suite {
+  expect(report.suites?.length ?? 0, 'Failed to fetch suite from report').toBeGreaterThan(idx);
+  return report.suites![idx];
+}
+
+function assertFirstTest(suite: FlakinessReport.Suite): FlakinessReport.Test {
+  const test = suite.tests?.at(0);
+  expect(test, `Failed to fetch first test from suite`).not.toBeUndefined();
+  return test!;
+}
+
 it('should work', async (ctx) => {
-  const result = await generateFlakinessReport(ctx, {
+  const { report } = await generateFlakinessReport(ctx, {
     'sum.test.ts': `
       import { expect, it } from 'vitest';
 
-      it('yo', async (ctx) => {
+      it('math should work', async (ctx) => {
         expect(1 + 1).toBe(2);
       });
     `
   });
-  expect(result.report.category).toBe('vitest');
+  expect(report.category).toBe('vitest');
+  const [suite] = assertSuites(report.suites, 1);
+  expect(suite.title).toBe('sum.test.ts');
+
+  const [test] = assertTests(suite, 1);
+  expect(test.title).toBe('math should work');
+  expect(test.attempts.length).toBe(1);
+  const [attempt] = test.attempts;
+  expect(attempt.status ?? 'passed').toBe('passed');
+  expect(attempt.expectedStatus ?? 'passed').toBe('passed');
+  expect(attempt.duration ?? 0).toBeGreaterThan(0);
+  expect(attempt.startTimestamp ?? 0).toBeGreaterThan(0);
 });
