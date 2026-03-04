cfd88ff208479e31d9376f8faf90c6071000ecf1
Andrey Lushnikov <aslushnikov@gmail.com>
2026-03-03 19:14:44 -0700

    test: set default timeout to 30s

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vitest/config';
 import path from 'path';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globalSetup: './tests/global-setup.ts',
     exclude: ['**/node_modules/**', '**/run-artifacts/**'],
     reporters: ['default', path.join(__dirname, './src/reporter.ts')],
+    testTimeout: 30000,
   },
 });
