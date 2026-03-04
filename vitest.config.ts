02b4e211a0ae99ebb900bdab6e955466cfe2f5d0
Andrey Lushnikov <aslushnikov@gmail.com>
2026-03-03 20:23:25 -0700

    devops: do not use tip-of-tree reportrer to run its own tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,9 @@
-import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globalSetup: './tests/global-setup.ts',
     exclude: ['**/node_modules/**', '**/run-artifacts/**'],
-    reporters: ['default', path.join(__dirname, './src/reporter.ts')],
     testTimeout: 30000,
   },
 });
