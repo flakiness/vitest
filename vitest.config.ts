fa58e5b19e1bae1bb91b703c2cf73217496b4b3c
Andrey Lushnikov <aslushnikov@gmail.com>
2026-03-05 11:48:44 -0700

    devops: dogfood vitest reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,15 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    includeTaskLocation: true,
     globalSetup: './tests/global-setup.ts',
     exclude: ['**/node_modules/**', '**/run-artifacts/**'],
     testTimeout: 30000,
+    reporters: [
+      ['default'],
+      ['@flakiness/vitest', {
+        flakinessProject: 'flakiness/vitest',
+      }],
+    ],
   },
 });
